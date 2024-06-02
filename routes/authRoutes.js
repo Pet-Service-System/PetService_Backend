21376903c3c10ef0349393934a1baa84aecd3420
MinhthnSE183110 <minhthnse183110@fpt.edu.vn>
2024-06-02 13:58:42 +0700

    resolve conflict

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { login, forgetPassword: resetPassword, changePassword } = require('../controllers/authController');
+const { login, register, forgetPassword, changePassword } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
-router.post('/forget-password', resetPassword);
+router.post('/forget-password', forgetPassword);
 router.post('/change-password', authMiddleware, changePassword);
 
 
