dd045b0eae3b4c474b91e9bdbb4f1fd8b2760704
Vũ Quang Minh - SE183085 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-08 22:38:59 +0700

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -3,7 +3,12 @@ const jwt = require('jsonwebtoken');
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
+
+  const token = authHeader.split(' ')[1]; // Ensure the token is in the format "Bearer <token>"
   if (!token) {
     return res.status(401).json({ message: 'No token provided' });
   }
@@ -14,10 +19,18 @@ const authMiddleware = (req, res, next) => {
       return res.status(401).json({ message: 'Failed to authenticate token' });
     }
 
-    req.user = decoded;
+    req.user = decoded; // Set the decoded payload to req.user
     next();
   });
 };
 
-module.exports = authMiddleware;
-
+// Middleware to check user roles
+const checkRole = (roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({ message: 'Access denied. You do not have permission to access this resource.' });
+    }
+    next();
+  };
+};
+module.exports = {authMiddleware, checkRole};
