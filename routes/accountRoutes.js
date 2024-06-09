3cb7acabf2cf4b545145275896ac269781980335
VuQuangMinh1212 <minhvqse183085@fpt.edu.vn>
2024-06-09 12:53:56 +0700

    fix bugs

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {authMiddleware, checkRole} = require('../middlewares/authMiddleware');
+const {authMiddleware} = require('../middlewares/authMiddleware');
 
 const {getAllAccounts, getAccount, getAccountById, getAccountByRole, updateAccount, updateAccountById, deleteAccount} = require('../controllers/accountController');
 
-router.get('/all', authMiddleware, checkRole(['admin']), getAllAccounts);
+router.get('/all', authMiddleware, getAllAccounts);
 router.get('/me', authMiddleware, getAccount);
-router.get(/:id/, authMiddleware, checkRole(['admin']), getAccountById);
+router.get(/:id/, authMiddleware, getAccountById);
 router.get('/role/:role?', getAccountByRole);
 router.patch('/:id', authMiddleware, updateAccount);
-router.patch('/:id', authMiddleware, checkRole(['admin']), updateAccountById);
-router.delete('/me', authMiddleware, checkRole(['admin']), deleteAccount);
+router.patch('/:id', authMiddleware, updateAccountById);
+router.delete('/me', authMiddleware, deleteAccount);
 
 module.exports = router;
 
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const {createProduct, getProducts, getProductById, updateProduct, deleteProduct , getProductsByPetType } = require('../controllers/productController');
-const {authMiddleware, checkRole} = require('../middlewares/authMiddleware');
+const {authMiddleware} = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', authMiddleware, checkRole(['manager']), createProduct);
+router.post('/', authMiddleware, createProduct);
 router.get('/', getProducts);
 router.get('/:id', getProductById);
-router.patch('/:id', authMiddleware, checkRole(['manager']), updateProduct);
+router.patch('/:id', authMiddleware, updateProduct);
 router.delete('/:id',  deleteProduct);
 router.get('/petType/:petTypeId', getProductsByPetType);
 module.exports = router;
