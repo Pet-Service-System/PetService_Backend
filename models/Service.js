47b956b46abf46b81839b5572348c387c28d9b52
MinhthnSE183110 <minhthnse183110@fpt.edu.vn>
2024-06-11 10:49:06 +0700

    change create-service and attributes name

--- models/Service.js
+++ models/Service.js
@@ -4,7 +4,7 @@ const serviceSchema = new mongoose.Schema({
     ServiceID: { type: String, required: true, unique: true },
     ServiceName: { type: String, required: true },
     Description: { type: String },
-    Image_URL: { type: String },
+    ImageURL: { type: String },
     Price: { type: Number, required: true },
     Status: { type: String }
 });
