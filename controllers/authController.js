9e2c34b2686dfd8445b792cae77cd4fe5e654b79
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-20 17:28:41 +0700

    fix login with google account

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || '1h';
 const EMAIL_USERNAME = process.env.EMAIL_USERNAME;
 const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CALLBACK_URL);
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 
 // Login api to authenticate the user and provide a JWT token
@@ -257,4 +257,47 @@ exports.resetPassword = async (req, res) => {
 exports.logout = (req, res) => {
   // When logout, clear the token in the client side
   res.status(200).json({ message: 'Logout successful' });
+};
+
+
+exports.googleAuth = async (req, res) => {
+  const { token: googleToken } = req.body ;
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: googleToken,
+      audience: process.env.GOOGLE_CLIENT_ID,
+    });
+    const payload = ticket.getPayload();
+    const { name, email } = payload;
+
+    let account = await Account.findOne({ email });
+
+    if (!account) {
+      account = new Account({
+        AccountID: await generateAccountID(),
+        fullname: name,
+        password: '', // No password for Google accounts
+        address: '',
+        email,
+        phone: '',
+        status: 1,
+        role: 'Customer',
+      });
+      console.log(account);
+      await account.save();
+    }
+
+    const jwtToken = jwt.sign(
+      { id: account.AccountID, email: account.email, fullname: account.fullname, role: account.role },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES_IN }
+    );
+    return res.json({ message: 'Login successful', user: {  id: account.AccountID, email: account.email, role: account.role, fullname: account.fullname, phone: account.phone, address: account.address },
+      token: jwtToken
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(400).json({ message: 'Error during authentication', error });
+  }
 };
