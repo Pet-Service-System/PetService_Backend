b6d14cd74947c76644090afcfeca80722976a774
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-24 15:49:42 +0700

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     OrderID: { type: String, required: true, unique: true },
     OrderDate: { type: Date, default: Date.now },
-    phone: { type: String, required: true },
+    Name: { type: String, required: true },
+    Phone: { type: String, required: true },
     Address: { type: String, required: true },
     Status: { type: String, required: true },
     TotalPrice: { type: Number, required: true },
