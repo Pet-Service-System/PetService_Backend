8946f17d03b61b8522dc1ba4d6be5cb95b6c5c6e
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-26 10:06:41 +0700

    Update SpaBookingDetails.js

--- models/SpaBookingDetails.js
+++ models/SpaBookingDetails.js
@@ -5,11 +5,8 @@ const spaBookingDetailsSchema = new Schema({
  BookingDetailsID: { type: String, required: true, unique: true },
  BookingID: { type: String, required: true, ref: 'SpaBooking' },
   ServiceID: { type: String, required: true, ref: 'SpaService' },
-  ServiceName: { type: String, required: true },
-  Price: { type: Number, required: true },
   BookingDate: {type: String, required: true},
   BookingTime: { type: String, required: true },
-  TotalPrice: { type: Number, required: true },
   Feedback: { type: String },
 });
 
