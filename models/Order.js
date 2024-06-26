18b6d929ba31a2f3c290c98de64ca9a5144e6495
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-26 10:24:50 +0700

    change order model and order details model

--- models/Order.js
+++ models/Order.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     OrderID: { type: String, required: true, unique: true },
     OrderDate: { type: Date, default: Date.now },
-    CustomerName: { type: String, required: true },
-    Phone: { type: String, required: true },
-    Address: { type: String, required: true },
-    AccountID: { type: String, required: true, ref: 'Account' },
     Status: { type: String, required: true },
+    TotalPrice: { type: Number, required: true },
   }, { versionKey: false },);
 
   
--- models/OrderDetails.js
+++ models/OrderDetails.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const OrderDetailsSchema = new mongoose.Schema({
     OrderDetailsID: { type: String, required: true, unique: true },
     OrderID: { type: String, required: true, ref: 'Order' },
+    CustomerName: { type: String, required: true },
+    Phone: { type: String, required: true },
+    Address: { type: String, required: true },
+    AccountID: { type: String, required: true, ref: 'Account' },
     OrderDetails: [{
     ProductID: { type: String, required: true, ref: 'Product' },
     Price: { type: Number, required: true },
