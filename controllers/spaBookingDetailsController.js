169e35c35112a94b897a78fc8a1930f422f93d3c
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-26 09:43:05 +0700

    ../controllers/spaBookingDetailsController.js

---
+++ controllers/spaBookingDetailsController.js
@@ -0,0 +1,81 @@
+const SpaBookingDetails = require('../models/SpaBookingDetails');
+
+// Create a new spa booking detail
+exports.createBookingDetail = async (req, res) => {
+    try {
+        const lastBookingDetails = await SpaBookingDetails.findOne().sort({ BookingDetailsID: -1 });
+    const newId = lastBookingDetails ? `SBD${String(parseInt(lastBookingDetails.BookingDetailsID.substring(2)) + 1).padStart(3, '0')}` : 'SBD001';
+        const newBookingDetail = new SpaBookingDetails({...req.body, BookingDetailsID: newId});
+        await newBookingDetail.save();
+        res.status(201).json(newBookingDetail);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Get all spa booking details
+exports.getAllBookingDetails = async (req, res) => {
+    try {
+        const bookingDetails = await SpaBookingDetails.find();
+        res.status(200).json(bookingDetails);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a single spa booking detail by booking details ID
+exports.getBookingDetailById = async (req, res) => {
+    try {
+        const bookingDetail = await SpaBookingDetails.findOne({BookingDetailsID: req.params.id});
+        if (bookingDetail) {
+            res.status(200).json(bookingDetail);
+        } else {
+            res.status(404).json({ message: 'Booking detail not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a single spa booking detail by booking details ID
+exports.getBookingDetailByBookingId = async (req, res) => {
+    try {
+        const bookingDetail = await SpaBookingDetails.findOne({BookingID: req.params.id});
+        if (bookingDetail) {
+            res.status(200).json(bookingDetail);
+        } else {
+            res.status(404).json({ message: 'Booking detail not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+
+// Update a spa booking detail by ID
+exports.updateBookingDetailById = async (req, res) => {
+    try {
+        const updatedBookingDetail = await SpaBookingDetails.findOneAndUpdate({BookingDetailsID:req.params.id}, req.body, { new: true });
+        if (updatedBookingDetail) {
+            res.status(200).json(updatedBookingDetail);
+        } else {
+            res.status(404).json({ message: 'Booking detail not found' });
+        }
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+// Delete a spa booking detail by ID
+exports.deleteBookingDetailById = async (req, res) => {
+    try {
+        const deletedBookingDetail = await SpaBookingDetails.findOneAndDelete({BookingDetailsID: req.params.id});
+        if (deletedBookingDetail) {
+            res.status(200).json({ message: 'Booking detail deleted' });
+        } else {
+            res.status(404).json({ message: 'Booking detail not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
