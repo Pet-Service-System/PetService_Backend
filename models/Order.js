0f0421f47a4c39c1c1c496b7ed0f6ca2efb2b5ee
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-27 20:13:07 +0700

    change orderDate from string to Date

--- models/Order.js
+++ models/Order.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   OrderID: { type: String, required: true, unique: true },
-  OrderDate: { type: String, default: Date.now },
+  OrderDate: { type: Date, default: Date.now },
   Status: { type: String, required: true },
   TotalPrice: { type: Number, required: true },
   AccountID: { type: String, required: true, ref: 'Account' },
