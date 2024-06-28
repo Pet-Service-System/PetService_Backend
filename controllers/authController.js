3864f443e2a2a4f1754a2717faea6629fa4c8b85
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-28 19:40:51 +0700

    update booking model

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
 const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const Account = require('../models/Account');
 const nodemailer = require('nodemailer');
