e96eaafb5e1c16e9830f5f203d25041beabed0be
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-06-30 11:54:25 +0700

    fix booking controller

--- models/SpaBooking.js
+++ models/SpaBooking.js
@@ -5,8 +5,8 @@ const spaBookingSchema = new Schema({
   BookingID: { type: String, required: true, unique: true },
   Status: { type: String, required: true },
   CreateDate: { type: Date, required: true },
-  TotalPrice: {type: Number, required: true},
   AccountID: { type: String, required: true, ref: 'Account' },
+  isReviewed: {type: Boolean, default: false},
 });
 
 const SpaBooking = mongoose.model('SpaBooking', spaBookingSchema, 'SpaBookings');
