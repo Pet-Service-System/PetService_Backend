b3727b651feedba01cbc576ec604cccd04130737
VuQuangMinh1212 <136490863+VuQuangMinh-SE183085@users.noreply.github.com>
2024-07-10 09:02:51 +0700

    create reply comment function

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {authMiddleware} = require('../middlewares/authMiddleware');
 
-const {getAllAccounts, getAccountById, getAccountByRole, updateAccountById, deleteAccount} = require('../controllers/accountController');
+const {getAllAccounts, getAccountById, getAccountByRole, updateAccountById, deleteAccount, getFullnameById} = require('../controllers/accountController');
 
 router.get('/role/:role?', getAccountByRole);
 router.get('/all', authMiddleware, getAllAccounts);
 router.get('/:id/', authMiddleware, getAccountById);
 router.patch('/:id', authMiddleware, updateAccountById);
 router.delete('/me', authMiddleware, deleteAccount);
-
+router.get('/fullname/:id', getFullnameById);
 module.exports = router;
