de79df8fce577b923b2d89871799d9cda0e502cc
MinhthnSE183110 <minhthnse183110@fpt.edu.vn>
2024-07-23 14:25:56 +0700

    remove comment

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -14,10 +14,9 @@ exports.getAllAccounts = async (req, res) => {
 
   // Get Account by ID 
   exports.getAccountById = async (req, res) => {
-    const accountId = req.params.id; // Assuming the account ID is provided in the request params
+    const accountId = req.params.id;
   
     try {
-      // Find the account by ID
       const account = await Account.findOne({AccountID: accountId});
   
       if (!account) {
@@ -33,16 +32,14 @@ exports.getAllAccounts = async (req, res) => {
 
 // Get Account by Role (admin)
 exports.getAccountByRole = async (req, res) => {
-  const role = req.params.role; // Assuming the role is provided in the request params
+  const role = req.params.role;
 
   try {
     let accounts;
 
     if (role) {
-      // Find accounts by specified role
       accounts = await Account.find({ role: role });
     } else {
-      // Find accounts by default roles
       accounts = await Account.find({ role: { $in: ['Sales Staff', 'Caretaker Staff'] } });
     }
 
@@ -71,8 +68,7 @@ exports.updateAccountById = async (req, res) => {
       if (!account) {
         return res.status(404).json({ message: 'Account not found!' });
       }
-  
-      // Update account properties
+
       Object.assign(account, updates);
   
       await account.save();
@@ -87,10 +83,9 @@ exports.updateAccountById = async (req, res) => {
   
   // Delete Account API (admin)
   exports.deleteAccount = async (req, res) => {
-    const accountId = req.user.id; // Assuming the user's account ID is stored in the request
+    const accountId = req.user.id;
   
     try {
-      // Find the account by ID and delete it
       const deletedAccount = await Account.findByIdAndDelete(accountId);
   
       if (!deletedAccount) {
